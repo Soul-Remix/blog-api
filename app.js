49a578e0635e05d9745a4f0af4d2abb0cf1eba64
Soul-Remix <soulremix-webdev@pm.me>
2021-10-10 15:01:28 +0300

    add file ext to files saved by multer

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const fileStorage = multer.diskStorage({
     cb(null, './images');
   },
   filename: (req, file, cb) => {
-    cb(null, uuidv4());
+    cb(null, uuidv4() + path.extname(file.originalname));
   },
 });
 
